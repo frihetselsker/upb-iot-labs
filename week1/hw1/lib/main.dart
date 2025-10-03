37bee127dc536f926833b00f06a1eb69bcdfafb6
Roi Bachynskyi <roybachynskyi@gmail.com>
2025-10-03 21:07:12 +0300

    finished hw1

--- week1/hw1/lib/main.dart
+++ week1/hw1/lib/main.dart
@@ -11,56 +11,74 @@ class Products extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: const Text('CyberMarket', style: TextStyle(color: Colors.white))),
+        title: Center(child: const Text('NASA Display', style: TextStyle(color: Colors.white))),
         backgroundColor: Colors.indigo,
       ),
-      body: Center(
+      body: Container(color: Colors.indigo[100],
+      child: Center(
         child: Column(
           children: <Widget>[
             Container(
               margin: const EdgeInsets.all(10.0),
               padding: const EdgeInsets.all(10.0),
               child: const Text(
-                "Welcome to Anonymous Store!",
-                style: TextStyle(color: Colors.red),
-              ),
-            ),
-            Expanded(
-              child: GridView.extent(
-                primary: false,
-                padding: const EdgeInsets.all(16),
-                crossAxisSpacing: 25,
-                mainAxisSpacing: 50,
-                maxCrossAxisExtent: 450.0,
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.network(
-                      'https://m.media-amazon.com/images/I/81+HS23yFvL._UY1000_.jpg',
-                      width: 300,
-                      height: 300,
-                      fit: BoxFit.cover,
-                      ),
-                      Text("The Circle")
-                    ],
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.network(
-                      'https://m.media-amazon.com/images/I/81wdgapPaPL._AC_SL1500_.jpg',
-                      width: 300,
-                      height: 300,
-                      fit: BoxFit.cover,
-                      ),
-                      Text("The Square")
-                    ],
-                  ),
-                ],
+                "Welcome to the website!",
+                style: TextStyle(color: Colors.red, fontSize: 32),
               ),
             ),
             Row(
+                spacing: 100.0,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 20,
+                      children: [
+                        Image.network(
+                        'https://upload.wikimedia.org/wikipedia/commons/e/e9/Apollo_6_launch.jpg',
+                        width: 300,
+                        height: 400,
+                        fit: BoxFit.cover,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10.0),
+                          margin: EdgeInsets.all(4.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.indigo
+                          ),
+                          child: const Text("Apollo 6", style:TextStyle(color: Colors.white)),
+                        )
+                      ],
+                    ),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 20,
+                      children: [
+                        Image.network(
+                        'https://upload.wikimedia.org/wikipedia/commons/4/4a/SpaceX_Starship_ignition_during_IFT-5.jpg',
+                        width: 300,
+                        height: 400,
+                        fit: BoxFit.cover,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10.0),
+                          margin: EdgeInsets.all(4.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.indigo
+                          ),
+                          child: const Text("Starship", style:TextStyle(color: Colors.white)),
+                        )
+                      ],
+                    ),
+                ],
+              ),
+            const SizedBox(height: 70),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 40,
               children: [
                 ElevatedButton(style: ElevatedButton.styleFrom(
                   backgroundColor: Color.fromARGB(255, 0, 255, 255),
@@ -81,107 +99,139 @@ class Products extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const RegisterPage(),
+                      builder: (context) => const RegistrationPage(),
                     ),
                   );
                 },
                 child: const Text('Register'),),
                 
             ],),
-            const SizedBox(height: 20),
           ],
         ),
+      ),
       ),
     );
   }
 }
 
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Image Screen'),
-        backgroundColor: Colors.blue,
+        title: const Center(child: Text('Login Page', style: TextStyle(color: Colors.white))),
+        backgroundColor: Colors.indigo,
       ),
-      body: SingleChildScrollView(
+      body: Container(
+        color: Colors.indigo[100],
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(16),
-                child: TextField(
-                  decoration: InputDecoration(hintText: 'TextField'),
+              Container(
+                width: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  border: Border.all()
                 ),
-              ),
-              Image.network(
-                'https://www.gstatic.com/flutter-onestack-prototype/genui/example_1.jpg',
-                width: 200,
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-              Stack(
-                children: [
-                  Container(
-                    width: 300,
-                    height: 200,
-                    color: Colors.yellow,
-                    child: Center(child: Text('Top Widget')),
-                  ),
-                  Positioned(
-                    top: 10,
-                    left: 50,
-                    child: Container(
-                      width: 80,
-                      height: 40,
-                      color: Colors.blue,
-                      child: Center(child: Text('Left Widget')),
-                    ),
-                  ),
-                  Positioned(
-                    top: 40,
-                    right: 10,
-                    child: Container(
-                      width: 80,
-                      height: 40,
-                      color: Colors.red,
-                      child: Center(child: Text('Right Widget')),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: Colors.brown,
-                    child: Center(child: Text('Row1')),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: Colors.brown,
-                    child: Center(child: Text('Row2')),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 50,
-                    color: Colors.brown,
-                    child: Center(child: Text('Row3')),
-                  ),
-                ],
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Email address'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Password'), 
+                          obscureText: true,
+                        ),
+                      )
+                    ],
+                 )
               ),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                child: Text('Go Back'),
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RegistrationPage extends StatelessWidget {
+  const RegistrationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Register Page', style: TextStyle(color: Colors.white))),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Container(
+        color: Colors.indigo[100],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  border: Border.all()
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'First Name'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Last Name'), 
+                          obscureText: true,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Email address'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                        child: TextField(
+                          decoration: InputDecoration(hintText: 'Password'), 
+                          obscureText: true,
+                        ),
+                      )
+                    ],
+                 )
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Submit'),
               ),
             ],
           ),
